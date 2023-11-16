41c88617923b40be00c87c54b290b69087e376cd
kosodrzewina <sienkiew@live.com>
2023-11-17 00:27:30 +0100

    moved getting Currency from string logic to Currency

--- Currency.java
+++ Currency.java
@@ -1,5 +1,30 @@
 public enum Currency {
     PLN,
     USD,
-    EUR
+    EUR;
+
+    public static Currency getFromString(String currencyString) {
+        var currency = getFromStringAsText(currencyString);
+        if (currency == null) {
+            currency = getFromStringAsNumber(currencyString);
+        }
+
+        return currency;
+    }
+
+    private static Currency getFromStringAsText(String currency) {
+        try {
+            return Currency.valueOf(currency);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    private static Currency getFromStringAsNumber(String currency) {
+        try {
+            return Currency.values()[Integer.parseInt(currency)];
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
 }
